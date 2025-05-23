92e14730cf3a47d66604808f5a8d3670b082e18e
bolro <bolro@sm1l0.local>
2025-05-23 13:18:25 +0800

    init

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <section className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="font-bold text-[40px]">Have a Great day!U MotherF!cker</h1>
-    </section>
+    <section className="w-full h-screen flex flex-col items-center justify-center"></section>
   );
 }
